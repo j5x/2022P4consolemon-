d96bbed03b8f905e2241e0e78e6121c3491f6858
j5x <32881@ma-web.nl>
2022-05-09 14:53:30 +0200

    End Product

---
+++ GDCSharp2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace GDCSharp2
+{
+    public class ConsoleMon
+    {
+        int Health = 100;
+        int Energy = 100;
+        string Name = "Jakko";
+        public void TakeDamage(int Damage)
+        {
+            Health = Health - Damage;
+        }
+        public void DepleteEnergy(int Energy)
+        {
+            this.Energy -= Energy;
+        }
+    }
+    
+
+    public class Skill
+    {
+        public int Damage = 100;
+        public int EnergyCost = 100;
+        public string Name = "StinkyBreathAttack";
+
+        public void UseOn(ConsoleMon target, ConsoleMon caster)
+        {
+            target.TakeDamage(Damage);
+            caster.DepleteEnergy(EnergyCost);
+            
+            
+        }
+    }
+}
